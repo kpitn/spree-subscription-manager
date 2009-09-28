7af85ee49f09430a189e79f3549cdd81d1d973fd
pierre <pierre@fx-pierre.(none)>
2009-09-28 11:23:52 +0200

    change mail

--- app/models/newsletter_mailer.rb
+++ app/models/newsletter_mailer.rb
@@ -2,11 +2,11 @@ class NewsletterMailer < ActionMailer::Base
    self.delivery_method = :activerecord
    layout 'newsletter'
 
-   def send_newsletter(newsletter,user)
+   def send_newsletter(newsletter,email)
     @subject    =  newsletter.subject
     @body       =  {"newsletter" => newsletter}
-    @recipients =  user.email
-    @from       = "newsletter@fxmodelrc.com"
+    @recipients =  email
+    @from       = "shop@fxmodelrc.com"
     @sent_on    = Time.now
     @content_type = "text/html"
     @headers["sender_id"]=newsletter.id
